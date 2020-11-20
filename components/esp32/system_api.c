00f462e62dc6fb048b0880fe60b906ac4a2e7a44
Christian Baars <Baars@gmx.de>
2020-11-20 15:59:06 +0100

    Update system_api.c

--- components/esp32/system_api.c
+++ components/esp32/system_api.c
@@ -114,11 +114,12 @@ esp_err_t esp_efuse_mac_get_default(uint8_t* mac)
             if ((mac_low >= 0x346a85c7) && (mac_low <= 0x346a85f8)) {
                 return ESP_OK;
             }
-            else if (esp_efuse_get_pkg_ver() ==  3) {
-                return ESP_OK; // override for Xiaomi SOC's and maybe others too
-            }
         } else {
             ESP_LOGE(TAG, "Base MAC address from BLK0 of EFUSE CRC error, efuse_crc = 0x%02x; calc_crc = 0x%02x", efuse_crc, calc_crc);
+            else if (esp_efuse_get_pkg_ver() ==  3) {
+                ESP_LOGE(TAG, "Found OEM-type ESP32, ignore error ...");
+                return ESP_OK; // override for Xiaomi SOC's and maybe others too
+            }
             abort();
         }
     }
